21d14d36d353830b6f17dc823130ec72ee107823
cisco00 <idokofrancis66@gmail.com>
2023-05-21 18:36:27 +0100

    updating

--- app_test.py
+++ app_test.py
@@ -1,5 +1,11 @@
 import pytest
 from main import hello_world
+from main import picking_crops
+
 
 def test_hello_world():
     assert hello_world() == "Hello World!"
+
+
+def test_crops_picked():
+    assert picking_crops() == dict()
