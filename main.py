38da3ce580d5f760d0a6b23b985d16627e8e27fa
Adefolarin Samson Adesoba <a.adesoba@native.semicolon.africa>
2023-05-23 21:31:03 +0100

    added final dataframe

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from typing import Any
 import pandas as pd
-from flask import Flask
+from flask import Flask, jsonify
 import pickle
 
 app = Flask(__name__)
@@ -12,7 +12,7 @@ crop_list = {"Yam": 0, "Maize": 1, "Sorghum": 2, "Cotton": 3, "Cassava": 4,
              "Rubber": 15, "MilletsSorghum": 16, "Plaintain": 17, "Acha": 18, "SugerCane": 19, "Yam.": 20,
              "MaizeCocoa": 21}
 
-final_crop = pd.read_csv('crops_dataset_model_building.csv')
+final_crop = pd.read_csv('models/final dataframe.csv')
 
 
 def farmers_input():
@@ -37,11 +37,33 @@ def getting_crop_index(crop):
 
 
 crop_index = getting_crop_index(value_crop)
+similarity = pickle.load(open('models/model.pkl', 'rb'))
+
+
+def get_crop_similarities(crop_index):
+    similar_crops = list(enumerate(similarity[crop_index]))
+    return sorted(similar_crops, key=lambda x: x[1], reverse=True)
+
+
+def get_title_from_index(index):
+    return final_crop[final_crop.index == index]["MAJOR_CROP"].values[0]
+
+
+lst = []
+
+
+def get_list_of_similar_crop():
+    i = 0
+    for index in get_crop_similarities(crop_index):
+        lst.append(get_title_from_index(index[0]))
+        i = i + 1
+        if i > 5:
+            break
 
 
 @app.route('/api/v1/recommend')
-def hello_world():  # put application's code here
-    return str(picking_crops(farmers_input()))
+def recommendation():
+    return jsonify(lst)
 
 
 if __name__ == '__main__':
