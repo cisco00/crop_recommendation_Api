fdcac21b4f9698edc58e45f60c92fd08b567cb3c
cisco00 <idokofrancis66@gmail.com>
2023-05-25 04:41:40 +0100

    updated app

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import pandas as pd
 from flask import Flask
+from sklearn.metrics.pairwise import cosine_similarity
+
 import pickle
 
 app = Flask(__name__)
@@ -15,9 +17,13 @@ state_list = {"adamawa": 0, "bauchi": 1, "bayelsa": 2, "benue": 3, "federal capi
               "kaduna": 5, "kano": 6, "katsina": 7, "kebbi": 8, "kogi": 9, "kwara": 10, "nasarawa": 11,
               "niger": 12, "plateau": 13, "taraba": 14}
 
-final_crop1 = pd.read_csv('crops_dataset_model_building.csv')
-final_crop = final_crop1.iloc[:, 1:]
-final_crop.head()
+
+data = pd.read_csv("data_index_file.csv")
+final_df = data.iloc[:, 1:]
+
+df1 = pd.read_csv('crops_dataset_model_building.csv')
+df2 = df1.iloc[:, 1:]
+model = cosine_similarity(df2)
 
 
 def farmers_input():
@@ -25,25 +31,12 @@ def farmers_input():
     return user_input
 
 
-def pickin_crops(crop):
-    crop_type = 0
-    for key, value in crop_list.items():
-        if crop == key:
-            crop_type = value
-        else:
-            pass
-
-    return crop_type
-
-
-def picking_crops(crop):
-    value = None
-    try:
-        value = crop_list[crop]
-    except KeyError:
-        print("The crop is not found in the list of crops trained with this model")
-
-    return value
+def picking_crop(crop):
+  if crop in crop_list:
+    value = crop_list[crop]
+  else:
+    return -1
+  return value
 
 
 def picking_state(state):
@@ -56,19 +49,46 @@ def picking_state(state):
     return states
 
 
-value_crop = pickin_crops(farmers_input)
+value_crop = picking_crop(farmers_input)
 
 
 def getting_crop_index(crop):
-    return final_crop[final_crop.MAJOR_CROP == crop]["index"].values[0]
+    try:
+        return final_df[final_df.MAJOR_CROP == crop]["index"].values[0]
+    except:
+        return None
 
 
 crop_index = getting_crop_index(value_crop)
 
+similar_crops = list(enumerate(model[crop_index]))
+sorted_similar_crop = sorted(similar_crops, key=lambda x: x[1], reverse=False)
+
+
+def get_crop_from_index(index):
+    return final_df[final_df.index == index]["MAJOR_CROP"].values[0]
+
+# lst = []
+# i=0
+# for crop in sorted_similar_crop:
+#     lst.append(get_crop_from_index(crop[0]))
+#     i=i+1
+#     if i>100:
+#         break
+# list(dict.fromkeys(lst))
+
 
 @app.route('/api/v1/recommend')
 def hello_world():  # put application's code here
-    return str(picking_crops(farmers_input()))
+    lst = []
+    i = 0
+    for crop in sorted_similar_crop:
+        lst.append(get_crop_from_index(crop[0]))
+        i = i + 1
+        if i > 100:
+            break
+    return str(list(dict.fromkeys(lst)))
+    # return str(picking_crops(farmers_input()))
 
 
 if __name__ == '__main__':
