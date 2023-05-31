f662a5c390978a6df73c3c07355e25195a88f0f3
Adefolarin Samson Adesoba <a.adesoba@native.semicolon.africa>
2023-05-31 15:23:06 +0100

    no changes

--- app.py
+++ app.py
@@ -14,8 +14,8 @@ jaccard_dist_state = pdist(crop_state_ct.values, metric='jaccard')
 square_jaccard_dist_state = squareform(jaccard_dist_state)
 jaccard_similarity_array_state = 1 - square_jaccard_dist_state
 
-state_distance_df = pd.DataFrame(jaccard_similarity_array_state, index=crop_state_ct.index, 
-                                columns=crop_state_ct.index)
+state_distance_df = pd.DataFrame(jaccard_similarity_array_state, index=crop_state_ct.index,
+                                 columns=crop_state_ct.index)
 
 # crop recommendation based on vegetation
 crop_vegetation_ct = pd.crosstab(df.MAJOR_CROP, df.VEGETATION)
@@ -25,8 +25,8 @@ jaccard_dist_vegetation = pdist(crop_vegetation_ct.values, metric='jaccard')
 square_jaccard_dist_vegetation = squareform(jaccard_dist_vegetation)
 jaccard_similarity_array_vegetation = 1 - square_jaccard_dist_vegetation
 
-vegetation_distance_df = pd.DataFrame(jaccard_similarity_array_vegetation, index=crop_vegetation_ct.index, 
-                                columns=crop_vegetation_ct.index)
+vegetation_distance_df = pd.DataFrame(jaccard_similarity_array_vegetation, index=crop_vegetation_ct.index,
+                                      columns=crop_vegetation_ct.index)
 
 
 @app.route('/api/v1/recommend/<crop>/<input>')
@@ -41,7 +41,8 @@ def make_recommendation(crop, input):
         #     return 'The inputed crop does not exist in the data'
     elif input == 'vegetation':
         # try:
-        vegetation_recommended_crops = vegetation_distance_df[crop.title()].sort_values(ascending=False)[:5].index.values
+        vegetation_recommended_crops = vegetation_distance_df[crop.title()].sort_values(ascending=False)[
+                                       :5].index.values
         vegetation_recommended_crops_list = list(vegetation_recommended_crops)
         predict_vegetation = df[df['MAJOR_CROP'].isin(vegetation_recommended_crops_list)]
         return str(predict_vegetation.VEGETATION.unique()[:10])
